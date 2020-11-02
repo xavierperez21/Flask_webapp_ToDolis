aec9ca7fae6483f82124c6a35bf069e63713b51d
Luis Xavier Perez <xavierpm1221@gmail.com>
2020-11-02 17:40:41 -0600

    Fixing some bugs

--- init_flask.sh
+++
@@ -1,9 +0,0 @@
-# !/bin/bash
-# Programm to initialize flask and its environment variables
-# Autor: Luis Xavier Pérez | xavierpm1221@gmail.com | 27 / 10 / 2020
-
-source ./venv/bin/activate
-
-export FLASK_APP=main.py
-export FLASK_DEBUG=1
-export FLASK_ENV=development
